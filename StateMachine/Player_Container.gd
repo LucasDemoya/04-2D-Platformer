9b5baa718ca1307f5dfd4a3534111baa7accae97
LucasDemoya <demoyal@iu.edu>
2022-11-02 23:21:45 -0400

    Very rough

--- StateMachine/Player_Container.gd
+++ StateMachine/Player_Container.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 onready var Player = load("res://Player/Player.tscn")
-var initial_position = Vector2(56,88)
+var starting_position = Vector2(200,200)
 
-func _process(_delta):
+
+func _physics_process(_delta):
 	if not has_node("Player"):
 		var player = Player.instance()
-		player.position = initial_position
-		player.name = "Player"
+		player.position = starting_position
 		add_child(player)
